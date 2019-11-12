4d4151776698c5e037be044bfcb7db9a906b30ef
Banu Kutlu <bzk60@psu.edu>
2019-11-12 09:59:46 -0500

    add a User model, check current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,14 @@
 class ApplicationController < ActionController::Base
 
   helper_method :symphony_client
+  def current_user
+    session_data = request.env['warden'].user
+    session_data && User.new(session_data)
+  end
+
+  def current_user?
+    current_user.present?
+  end
 
   def symphony_client
     @symphony_client ||= SymphonyClient.new
