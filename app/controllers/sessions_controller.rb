3cb971d3cbd919a88345682b4efacccad0d1d7ac
Banu Kutlu <bzk60@psu.edu>
2019-11-14 10:08:46 -0500

    remove commented out lines

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,9 +2,6 @@
 
 class SessionsController < ApplicationController
   def index
-
     redirect_to summaries_url if current_user? || request.env['warden'].authenticate(:library_id)
-    # TODO: redirect_to errors_url, alert: 'Unable to authenticate.'
-    # TODO: flash.now.alert = request.env['warden'].message
   end
 end
