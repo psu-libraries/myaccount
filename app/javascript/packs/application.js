9d9bd63ff391d678072f91b00bba071c40baa746
Charlie Morris <523381+cdmo@users.noreply.github.com>
2019-11-22 10:58:51 -0500

    Webpacker (#27)
    * Result of running `bundle exec rails webpacker:install`
    * Adds: FontAwesome 5, Twitter Bootstrap 4, jQuery (peer dependency of Twitter Bootstrap), Popper (peer dependency of Twitter Bootstrap), Turbolinks, and our standard typefaces
    * Removes mailer views
    * Adds our base theme stuff, adds in vendor resources.
    * Adds some html to homepage, very basic
    * specify version of nodejs
    * Seems like docs were not right from travis, so trying a more heavy-handed approach

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,15 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+// Vendor
+import 'bootstrap/dist/js/bootstrap'
+
+import './styles'
+
+// Fonts
+import 'typeface-open-sans'
+import 'typeface-roboto-slab'
+
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
