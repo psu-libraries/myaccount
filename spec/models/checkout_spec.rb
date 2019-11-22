cf5dc459686159e4ff28e09bcf55d46224123399
Banu Hapeloglu Kutlu <bzk60@psu.edu>
2019-11-22 16:47:27 -0500

    initial account overview work (#29)
    * BibRecord and Hold models
    * holds to summary
    * adding Checkout model and number of checkouts to summary
    * clean up
    * user and patron model specs
    * checkout spec
    * exclude BlockLength :cop: for specs

---
+++ spec/models/checkout_spec.rb
@@ -0,0 +1,121 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Checkout, type: :model do
+  subject(:checkout) do
+    described_class.new({
+      key: '1',
+      fields: fields
+    }.with_indifferent_access)
+  end
+
+  let(:fields) do
+    {
+      status: 'ACTIVE',
+      overdue: true,
+      checkOutDate: '2019-07-08T21:28:00-07:00',
+      dueDate: '2019-07-09T23:59:00-07:00',
+      library: {
+        key: 'UP-PAT'
+      },
+      patron: {
+        key: '123'
+      },
+      item: {
+        fields: {
+          barcode: 'xyz',
+          currentLocation: { key: 'CHECKEDOUT' },
+          bib: {
+            key: '123456',
+            fields: {
+              title: 'Some Title',
+              author: 'Somebody'
+            }
+          },
+          call: {
+            fields: {
+              dispCallNumber: 'ABC 123',
+              sortCallNumber: 'ABC 00123'
+            }
+          }
+        }
+      }
+    }
+  end
+
+  it 'has a key' do
+    expect(checkout.key).to eq '1'
+  end
+
+  it 'has a patron key' do
+    expect(checkout.patron_key).to eq '123'
+  end
+
+  it 'has a status' do
+    expect(checkout.status).to eq 'ACTIVE'
+  end
+
+  it 'has an overdue state' do
+    expect(checkout.overdue?).to be true
+  end
+
+  it 'does not have a recalled date' do
+    expect(checkout.recalled_date).to be_nil
+  end
+
+  it 'is not recalled' do
+    expect(checkout).not_to be_recalled
+  end
+
+  context 'with a record that has been recalled' do
+    before do
+      fields[:recalledDate] = '2019-07-11T13:59:00-07:00'
+      fields[:recallDueDate] = '2019-08-11T13:59:00-07:00'
+    end
+
+    it 'has a recalled date' do
+      expect(checkout.recalled_date.strftime('%m/%d/%Y')).to eq '07/11/2019'
+    end
+
+    it 'is recalled' do
+      expect(checkout).to be_recalled
+    end
+  end
+
+  it 'has a library' do
+    expect(checkout.library).to eq 'UP-PAT'
+  end
+
+  it 'has a title' do
+    expect(checkout.title).to eq 'Some Title'
+  end
+
+  it 'has an author' do
+    expect(checkout.author).to eq 'Somebody'
+  end
+
+  it 'has a call number' do
+    expect(checkout.call_number).to eq 'ABC 123'
+  end
+
+  it 'has a shelf key' do
+    expect(checkout.shelf_key).to eq 'ABC 00123'
+  end
+
+  it 'has a barcode' do
+    expect(checkout.barcode).to eq 'xyz'
+  end
+
+  it 'has a catkey' do
+    expect(checkout.catkey).to eq '123456'
+  end
+
+  it 'has a current location' do
+    expect(checkout.current_location).to eq 'CHECKEDOUT'
+  end
+
+  it 'is not lost' do
+    expect(checkout).not_to be_lost
+  end
+end
--- spec/models/patron_spec.rb
+++ spec/models/patron_spec.rb
@@ -3,5 +3,64 @@
 require 'rails_helper'
 
 RSpec.describe Patron, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:patron) do
+    described_class.new(
+      {
+        key: '1',
+        fields: fields
+      }.with_indifferent_access
+    )
+  end
+
+  let(:fields) do
+    {
+      firstName: 'Student',
+      lastName: 'Borrower',
+      barcode: '1234'
+    }
+  end
+
+  it 'has a key' do
+    expect(patron.key).to eq '1'
+  end
+
+  it 'has a first name' do
+    expect(patron.first_name).to eq 'Student'
+  end
+
+  it 'has a last name' do
+    expect(patron.last_name).to eq 'Borrower'
+  end
+
+  it 'has a display name' do
+    expect(patron.display_name).to eq 'Student Borrower'
+  end
+
+  it 'has a barcode' do
+    expect(patron.barcode).to eq '1234'
+  end
+
+  context 'with checkouts' do
+    before do
+      fields[:circRecordList] = [{ key: 1, fields: {} }]
+    end
+
+    describe '#checkouts' do
+      it 'returns a list of checkouts for the patron' do
+        expect(patron.checkouts).to include a_kind_of(Checkout).and(have_attributes(key: 1))
+      end
+    end
+  end
+
+  context 'with holds' do
+    before do
+      fields[:holdRecordList] = [{ key: 1, fields: {} }]
+    end
+
+    describe '#holds' do
+      it 'returns a list of holds for the patron' do
+        expect(patron.holds).to include a_kind_of(Hold).and(have_attributes(key: 1))
+      end
+    end
+  end
 end
