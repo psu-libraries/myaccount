80a7fc54f7a2da02f5c3de24f0164ccb57cbe0fb
Banu Kutlu <bzk60@psu.edu>
2019-11-22 17:25:55 -0500

    update describe

--- spec/views/summaries/index.html.erb_spec.rb
+++ spec/views/summaries/index.html.erb_spec.rb
@@ -2,6 +2,6 @@
 
 require 'rails_helper'
 
-RSpec.describe 'summaries/index.html.erb', type: :view do
+RSpec.describe 'summaries/index', type: :view do
   pending "add some examples to (or delete) #{__FILE__}"
 end
