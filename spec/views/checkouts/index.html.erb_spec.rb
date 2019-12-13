21169f02e2a073d453944a376e1c9a500da2abcc
Banu Kutlu <bzk60@psu.edu>
2019-12-13 13:56:50 -0500

    checkouts view spec

---
+++ spec/views/checkouts/index.html.erb_spec.rb
@@ -0,0 +1,88 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'checkouts/index.html.erb' do
+  context 'when a single checkout exists' do
+    let(:checkout) { build(:checkout) }
+
+    before do
+      assign(:checkouts, [checkout])
+    end
+
+    it 'displays total checked out correctly' do
+      render
+      expect(rendered).to include 'Total Checked Out: 1'
+    end
+
+    it 'displays total # of recalled items correctly' do
+      checkout.record['fields']['recalledDate'] = '2019-12-10'
+      render
+      expect(rendered).to include 'Recalled: 1'
+    end
+
+    it 'displays total # of overdue items correctly' do
+      checkout.record['fields']['overdue'] = true
+      render
+      expect(rendered).to include 'Overdue: 1'
+    end
+
+    it 'displays checkout\'s item\'s title / author' do
+      checkout.record['fields']['item']['fields']['bib']['fields']['title'] = 'A wonderful title'
+      checkout.record['fields']['item']['fields']['bib']['fields']['author'] = 'A wonderful author'
+      render
+      expect(rendered).to include 'A wonderful title / A wonderful author'
+    end
+
+    it 'displays checkout\'s item\'s canonical item link' do
+      checkout.record['fields']['item']['fields']['bib']['key'] = '123456'
+      render
+      expect(rendered).to include 'catalog.libraries.psu.edu/catalog/123456'
+    end
+
+    it 'displays checkout\'s item\'s item type' do
+      checkout.record['fields']['item']['fields']['itemType']['key'] = 'PERIODICAL'
+      render
+      expect(rendered).to include 'Bound Journal'
+    end
+
+    it 'displays checkout\'s renewal count' do
+      checkout.record['fields']['renewalCount'] = 2
+      render
+      expect(rendered).to include '<td>2</td>'
+    end
+
+    it 'displays checkout\'s due date correctly when not recalled' do
+      checkout.record['fields']['dueDate'] = '2019-11-14T23:59:00-05:00'
+      checkout.record['fields']['recalledDate'] = nil
+      render
+      expect(rendered).to include '<span>November 14, 2019 11:59pm</span>'
+    end
+
+    it 'displays checkout\'s due date correctly when recalled' do
+      checkout.record['fields']['dueDate'] = '2019-11-14T23:59:00-05:00'
+      checkout.record['fields']['recallDueDate'] = '2019-12-20T23:59:00-05:00'
+      render
+      expect(rendered).to include '<span>Recalled<br>December 20, 2019 11:59pm<br>November 14, 2019 11:59pm</span>'
+    end
+
+    it 'displays checkout\'s status correctly when overdue' do
+      checkout.record['fields']['overdue'] = true
+      render
+      expect(rendered).to include 'Overdue'
+    end
+
+    it 'displays checkout\'s status correctly when claims returned' do
+      checkout.record['fields']['overdue'] = true
+      checkout.record['fields']['claimsReturnedDate'] = '2019-11-01'
+      render
+      expect(rendered).to include 'Claims Returned'
+    end
+
+    it 'displays checkout\'s estimated overdue amount when item is overdue' do
+      checkout.record['fields']['estimatedOverdueAmount']['amount'] = '10.00'
+      render
+      expect(rendered).to include '$10.00'
+    end
+  end
+end
