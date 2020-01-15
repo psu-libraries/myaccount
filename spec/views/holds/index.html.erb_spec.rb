d2ffd1ff6bdaa58917826fecfc7c1d0d730b908e
Charlie Morris <523381+cdmo@users.noreply.github.com>
2020-01-15 13:39:09 -0500

    Adds conditional displays for holds (#71)

---
+++ spec/views/holds/index.html.erb_spec.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'holds/index.html.erb', type: :view do
+  let(:hold) { build(:hold) }
+
+  context 'when there are holds ready to pickup' do
+    before do
+      assign(:holds_ready, [hold])
+    end
+
+    it 'displays a hold' do
+      hold.record['fields']['bib']['fields']['title'] = 'A wonderful title'
+      render
+      expect(rendered).to include 'A wonderful title'
+    end
+
+    describe 'holds page' do
+      it 'has no holds that are not ready for pickup' do
+        render
+        expect(rendered).not_to include 'Holds that are not ready for pickup'
+      end
+
+      it 'has only holds ready for pickup' do
+        render
+        expect(rendered).to include 'Holds that are ready for pickup'
+      end
+    end
+  end
+
+  context 'when there are only holds not ready to pickup' do
+    before do
+      assign(:holds_not_ready, [hold])
+    end
+
+    describe 'holds page' do
+      it 'has only holds not ready for pickup' do
+        render
+        expect(rendered).not_to include 'Holds that are ready for pickup'
+      end
+
+      it 'has only holds ready for pickup' do
+        render
+        expect(rendered).to include 'Holds that are not ready for pickup'
+      end
+    end
+  end
+
+  context 'when there are only no holds' do
+    describe 'holds page' do
+      it 'shows a message that there aren\'t holds' do
+        render
+        expect(rendered).to include 'You have no holds at this time'
+      end
+    end
+  end
+end
