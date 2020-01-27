02161f942dfe3826b3bbd160d69b78ba3ea1d414
Charlie Morris <523381+cdmo@users.noreply.github.com>
2020-01-27 15:45:32 -0500

    Refactors summaries, adds new ActionView Component for Dashboard Item (#81)
    * Refactors summaries, adds new ActionView Component for Dashboard Item
    * Refactors spec
    * Adds ability to transform currency
    
    Co-authored-by: Banu Hapeloglu Kutlu <bzk60@psu.edu>

---
+++ app/components/dashboard_item_component.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+class DashboardItemComponent < ActionView::Component::Base
+  def initialize(model:, count:, count_term: nil, currency: nil, items: nil)
+    @model = model
+    @count = count
+    @currency = currency
+    @count_term = count_term
+    @items = items
+  end
+
+  def card_body
+    return nil if @items.nil?
+
+    facts = @items.map do |item|
+      content_tag :li, "#{item[:count]}  #{item[:label]}"
+    end.join
+
+    content_tag :ul, facts, nil, false
+  end
+
+  def total
+    return @count unless @currency
+
+    number_to_currency(@count)
+  end
+
+  def item_link
+    url = url_for(controller: @model.to_s.downcase, action: 'index')
+
+    link_to "See all #{@model}", url, class: 'btn btn-primary float-right'
+  end
+
+  def render?
+    @count.positive?
+  end
+end
