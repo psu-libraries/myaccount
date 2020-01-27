fb675055f250ba44bb4322f82c721c8e6786612f
Charlie Morris <cdm32@psu.edu>
2020-01-27 15:08:16 -0500

    Refactors spec
    Co-authored-by: Banu Kutlu <bzk60@psu.edu>

--- spec/components/dashboard_item_component_spec.rb
+++ spec/components/dashboard_item_component_spec.rb
@@ -8,34 +8,36 @@ RSpec.describe DashboardItemComponent, type: :component do
                                 count_term: 'Total checked out',
                                 items: [
                                   {
-                                    label: 'recalled',
-                                    count: 2
-                                  },
-                                  {
                                     label: 'overdue',
                                     count: 3
                                   }
                                 ] }}
+  let(:component) { render_inline(described_class, dashboard_item_args).to_html }
 
   it 'makes a list' do
-    component = render_inline(described_class, dashboard_item_args).to_html
+    dashboard_item_args[:items] << {
+        label: 'recalled',
+        count: 2
+    }
+
     expect(component).to include '<li>2  recalled</li>'
   end
 
   it 'links to a model path' do
-    component = render_inline(described_class, dashboard_item_args).to_html
-    expect(component).to include 'href="/checkouts"'
+    dashboard_item_args[:model] = "Holds"
+
+    expect(component).to include 'href="/holds"'
   end
 
   it 'doesn\'t render if count is not positive' do
     dashboard_item_args[:count] = 0
-    component = render_inline(described_class, dashboard_item_args).to_html
+
     expect(component).to be_empty
   end
 
   it 'doesn\'t render items if there aren\'t any items' do
     dashboard_item_args[:items] = nil
-    component = render_inline(described_class, dashboard_item_args).to_html
+
     expect(component).not_to include '<ul>'
   end
 end
