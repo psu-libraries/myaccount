10cf7f1c7b50515cac0dba48fabe45fb90ce8de6
Charlie Morris <523381+cdmo@users.noreply.github.com>
2020-02-03 09:13:42 -0500

    Makes routes more specific, exact (#104)

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@
 Rails.application.routes.draw do
   root to: 'sessions#index'
 
-  resources :summaries, :fines, :checkouts, :holds
-  resources :renewals, only: :create
+  resources :summaries, :fines, :checkouts, only: [:index]
+  resources :renewals, only: [:create]
+  resources :holds, only: [:index, :update, :destroy]
 
   get '/logout', to: 'sessions#destroy', as: :logout
 end
