be4dcb96ce50a5cde3d8333f3c6ac43a488f612c
Charlie Morris <cdm32@psu.edu>
2020-02-10 10:23:17 -0500

    Bib model spec

--- spec/models/bib_spec.rb
+++ spec/models/bib_spec.rb
@@ -3,6 +3,16 @@
 require 'rails_helper'
 
 RSpec.describe Bib, type: :model do
-  subject(:bib)  { build(:bib) }
+  subject(:bib) { build(:bib_with_volumetrics) }
+
+  it 'will generate new holds when supplied with a body that has a callList' do
+    results = Bib::generate_holds(bib.body)
+
+    expect(results.count).to be 8
+  end
+
+  it 'will show you an author' do
+    expect(bib.author).to be 'Hill Street blues (Television program)'
+  end
 
 end
