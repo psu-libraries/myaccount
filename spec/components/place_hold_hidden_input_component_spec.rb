8219c3fdc5e79adaab26363b384c529d44142977
Charlie Morris <cdm32@psu.edu>
2020-02-10 13:59:46 -0500

    hidden input spec

--- spec/components/place_hold_hidden_input_component_spec.rb
+++ spec/components/place_hold_hidden_input_component_spec.rb
@@ -3,14 +3,16 @@
 require 'rails_helper'
 
 RSpec.describe PlaceHoldHiddenInputComponent, type: :component do
-  let(:bib) { build(:bib_with_volumetrics) }
-  let(:component) { render_inline(described_class, bib: bib).to_html }
+  let(:bib_with_volumetrics) { build(:bib_with_volumetrics) }
+  let(:bib_without_volumetrics) { build(:bib_without_volumetrics) }
+  let(:component) { render_inline(described_class, bib: bib_without_volumetrics).to_html }
 
   it 'renders when volumetrics aren\'t detected' do
     expect(component).to_not be_empty
   end
 
   it 'does not render when volumetrics are detected' do
+    component = render_inline(described_class, bib: bib_with_volumetrics).to_html
     expect(component).to be_empty
   end
 end
--- spec/factories/bibs.rb
+++ spec/factories/bibs.rb
@@ -2,6 +2,168 @@
 
 FactoryBot.define do
   factory :bib do
+    factory :bib_without_volumetrics do
+      body {
+            {"resource"=>"/catalog/bib",
+             "key"=>"26141494",
+             "fields"=>
+                 {"shadowed"=>false,
+                  "author"=>"Diamond, Jared M. author.",
+                  "titleControlNumber"=>"l2018952825",
+                  "catalogDate"=>"2019-05-28",
+                  "catalogFormat"=>{"resource"=>"/policy/catalogFormat", "key"=>"MARC"},
+                  "modifiedDate"=>"2019-12-08",
+                  "systemModifiedDate"=>"2020-02-04T15:11:00-05:00",
+                  "title"=>"Upheaval : turning points for nations in crisis",
+                  "callList"=>
+                      [{"resource"=>"/catalog/call",
+                        "key"=>"26141494:4",
+                        "fields"=>
+                            {"library"=>{"resource"=>"/policy/library", "key"=>"UP-PAT"},
+                             "callNumber"=>"HN13.D52 2019",
+                             "shadowed"=>false,
+                             "volumetric"=>nil,
+                             "dispCallNumber"=>"HN13.D52 2019",
+                             "bib"=>{"resource"=>"/catalog/bib", "key"=>"26141494"},
+                             "itemList"=>
+                                 [{"resource"=>"/catalog/item",
+                                   "key"=>"26141494:4:1",
+                                   "fields"=>
+                                       {"call"=>{"resource"=>"/catalog/call", "key"=>"26141494:4"},
+                                        "mediaDesk"=>nil,
+                                        "itemType"=>{"resource"=>"/policy/itemType", "key"=>"BOOK"},
+                                        "library"=>{"resource"=>"/policy/library", "key"=>"UP-PAT"},
+                                        "shadowed"=>false,
+                                        "homeLocation"=>
+                                            {"resource"=>"/policy/location", "key"=>"LEISURE-PA"},
+                                        "copyNumber"=>1,
+                                        "bib"=>{"resource"=>"/catalog/bib", "key"=>"26141494"},
+                                        "barcode"=>"000080793182",
+                                        "circulate"=>false,
+                                        "currentLocation"=>
+                                            {"resource"=>"/policy/location", "key"=>"CHECKEDOUT"}}},
+                                  {"resource"=>"/catalog/item",
+                                   "key"=>"26141494:4:2",
+                                   "fields"=>
+                                       {"call"=>{"resource"=>"/catalog/call", "key"=>"26141494:4"},
+                                        "mediaDesk"=>nil,
+                                        "itemType"=>{"resource"=>"/policy/itemType", "key"=>"BOOK"},
+                                        "library"=>{"resource"=>"/policy/library", "key"=>"UP-PAT"},
+                                        "shadowed"=>false,
+                                        "homeLocation"=>
+                                            {"resource"=>"/policy/location", "key"=>"PATERNO-2"},
+                                        "copyNumber"=>2,
+                                        "bib"=>{"resource"=>"/catalog/bib", "key"=>"26141494"},
+                                        "barcode"=>"000081297085",
+                                        "circulate"=>false,
+                                        "currentLocation"=>
+                                            {"resource"=>"/policy/location", "key"=>"ILLEND"}}}],
+                             "classification"=>
+                                 {"resource"=>"/policy/classification", "key"=>"LC"}}},
+                       {"resource"=>"/catalog/call",
+                        "key"=>"26141494:6",
+                        "fields"=>
+                            {"library"=>{"resource"=>"/policy/library", "key"=>"ALTOONA"},
+                             "callNumber"=>"HN13.D52 2019",
+                             "shadowed"=>false,
+                             "volumetric"=>nil,
+                             "dispCallNumber"=>"HN13.D52 2019",
+                             "bib"=>{"resource"=>"/catalog/bib", "key"=>"26141494"},
+                             "itemList"=>
+                                 [{"resource"=>"/catalog/item",
+                                   "key"=>"26141494:6:1",
+                                   "fields"=>
+                                       {"call"=>{"resource"=>"/catalog/call", "key"=>"26141494:6"},
+                                        "mediaDesk"=>nil,
+                                        "itemType"=>
+                                            {"resource"=>"/policy/itemType", "key"=>"BOOKFLOAT"},
+                                        "library"=>{"resource"=>"/policy/library", "key"=>"ALTOONA"},
+                                        "shadowed"=>false,
+                                        "homeLocation"=>
+                                            {"resource"=>"/policy/location", "key"=>"STACKS-AA"},
+                                        "copyNumber"=>1,
+                                        "bib"=>{"resource"=>"/catalog/bib", "key"=>"26141494"},
+                                        "barcode"=>"000081321605",
+                                        "circulate"=>false,
+                                        "currentLocation"=>
+                                            {"resource"=>"/policy/location", "key"=>"ILLEND"}}}],
+                             "classification"=>
+                                 {"resource"=>"/policy/classification", "key"=>"LC"}}},
+                       {"resource"=>"/catalog/call",
+                        "key"=>"26141494:3",
+                        "fields"=>
+                            {"library"=>{"resource"=>"/policy/library", "key"=>"BEHREND"},
+                             "callNumber"=>"HN13.D52 2019",
+                             "shadowed"=>false,
+                             "volumetric"=>nil,
+                             "dispCallNumber"=>"HN13.D52 2019",
+                             "bib"=>{"resource"=>"/catalog/bib", "key"=>"26141494"},
+                             "itemList"=>
+                                 [{"resource"=>"/catalog/item",
+                                   "key"=>"26141494:3:1",
+                                   "fields"=>
+                                       {"call"=>{"resource"=>"/catalog/call", "key"=>"26141494:3"},
+                                        "mediaDesk"=>nil,
+                                        "itemType"=>
+                                            {"resource"=>"/policy/itemType", "key"=>"BOOKFLOAT"},
+                                        "library"=>{"resource"=>"/policy/library", "key"=>"BEHREND"},
+                                        "shadowed"=>false,
+                                        "homeLocation"=>
+                                            {"resource"=>"/policy/location", "key"=>"STACKS-BD"},
+                                        "copyNumber"=>1,
+                                        "bib"=>{"resource"=>"/catalog/bib", "key"=>"26141494"},
+                                        "barcode"=>"000081287932",
+                                        "circulate"=>false,
+                                        "currentLocation"=>
+                                            {"resource"=>"/policy/location", "key"=>"ILLEND"}}}],
+                             "classification"=>
+                                 {"resource"=>"/policy/classification", "key"=>"LC"}}},
+                       {"resource"=>"/catalog/call",
+                        "key"=>"26141494:5",
+                        "fields"=>
+                            {"library"=>{"resource"=>"/policy/library", "key"=>"WSCRANTON"},
+                             "callNumber"=>"HN13.D52 2019",
+                             "shadowed"=>false,
+                             "volumetric"=>nil,
+                             "dispCallNumber"=>"HN13.D52 2019",
+                             "bib"=>{"resource"=>"/catalog/bib", "key"=>"26141494"},
+                             "itemList"=>
+                                 [{"resource"=>"/catalog/item",
+                                   "key"=>"26141494:5:1",
+                                   "fields"=>
+                                       {"call"=>{"resource"=>"/catalog/call", "key"=>"26141494:5"},
+                                        "mediaDesk"=>nil,
+                                        "itemType"=>
+                                            {"resource"=>"/policy/itemType", "key"=>"BOOKFLOAT"},
+                                        "library"=>{"resource"=>"/policy/library", "key"=>"WSCRANTON"},
+                                        "shadowed"=>false,
+                                        "homeLocation"=>
+                                            {"resource"=>"/policy/location", "key"=>"STACKS-WS"},
+                                        "copyNumber"=>1,
+                                        "bib"=>{"resource"=>"/catalog/bib", "key"=>"26141494"},
+                                        "barcode"=>"000081402335",
+                                        "circulate"=>true,
+                                        "currentLocation"=>
+                                            {"resource"=>"/policy/location", "key"=>"INTRANSIT"}}}],
+                             "classification"=>
+                                 {"resource"=>"/policy/classification", "key"=>"LC"}}},
+                       {"resource"=>"/catalog/call",
+                        "key"=>"26141494:1",
+                        "fields"=>
+                            {"library"=>{"resource"=>"/policy/library", "key"=>"WSCRANTON"},
+                             "callNumber"=>"SN15687372",
+                             "shadowed"=>false,
+                             "volumetric"=>nil,
+                             "dispCallNumber"=>"SN15687372",
+                             "bib"=>{"resource"=>"/catalog/bib", "key"=>"26141494"},
+                             "itemList"=>[],
+                             "classification"=>
+                                 {"resource"=>"/policy/classification", "key"=>"LC"}}}],
+                  "createDate"=>"2019-03-11"}}
+      }
+      initialize_with { new(attributes[:body]) }
+
+    end
 
     factory :bib_with_volumetrics do
       body {
