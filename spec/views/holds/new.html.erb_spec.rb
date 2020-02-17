90e60f49af20ee7927ca9d70298f36b596e75790
Charlie Morris <cdm32@psu.edu>
2020-02-17 14:55:27 -0500

    Sets default pickup by date to 2 months from now #110

--- spec/views/holds/new.html.erb_spec.rb
+++ spec/views/holds/new.html.erb_spec.rb
@@ -5,12 +5,15 @@ require 'rails_helper'
 RSpec.describe 'holds/new.html.erb', type: :view do
   let(:bib) { build(:bib_with_holdables) }
 
-  context 'with a new hold with holdables to choose' do
+  context 'during a request for a new hold with holdables to choose' do
     before do
-      assign(:bib, bib)
+     assign(:bib, bib)
     end
 
-    xit 'sets the pickup by date to two months from now' do
+    it 'sets the pickup by date to two months from now' do
+      render
+      expect(rendered).to include '<input value="2020-04-17" type="date" name="hold[pickup_by_date]"'\
+                                  ' id="hold_pickup_by_date" />'
     end
   end
 end
