7bfecb096339e35f0d7a47a9f75b867065792bc8
Banu Hapeloglu Kutlu <bzk60@psu.edu>
2020-03-03 13:40:44 -0600

    Update config/routes.rb
    Co-Authored-By: Charlie Morris <523381+cdmo@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -12,10 +12,10 @@ Rails.application.routes.draw do
   get '/logout', to: 'sessions#destroy', as: :logout
 
   # error pages
-  match '/404' => 'errors#not_found', via: :all
-  match '/422' => 'errors#not_found', via: :all
-  match '/500' => 'errors#internal_server_error', via: :all
+  match '404', to: 'errors#not_found', via: :all
+  match '422', to: 'errors#not_found', via: :all
+  match '500', to: 'errors#internal_server_error', via: :all
 
   # catchall for not predefined requests - keep this at the very bottom of the routes file
-  match '*catch_unknown_routes' => 'errors#not_found', via: :all
+  match '*catch_unknown_routes', to: 'errors#not_found', via: :all
 end
