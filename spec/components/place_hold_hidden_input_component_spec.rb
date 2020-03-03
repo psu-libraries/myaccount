1d2c8e1eeaabd3f57360b9e397b50af093b4244d
Charlie Morris <523381+cdmo@users.noreply.github.com>
2020-03-03 09:12:15 -0500

    Update spec/components/place_hold_hidden_input_component_spec.rb
    Co-Authored-By: Banu Hapeloglu Kutlu <bzk60@psu.edu>

--- spec/components/place_hold_hidden_input_component_spec.rb
+++ spec/components/place_hold_hidden_input_component_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe PlaceHoldHiddenInputComponent, type: :component do
-  it 'renders when there\s a barcode' do
+  it 'renders when there\'s a barcode' do
     component = render_inline(described_class, barcode: '1').to_html
     expect(component).not_to be_empty
   end
