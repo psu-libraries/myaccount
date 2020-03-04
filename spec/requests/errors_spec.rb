d3adf48c6720c7e283250a10086f44c14ffcb825
Banu Kutlu <bzk60@psu.edu>
2020-03-04 16:46:39 -0500

    addied examples for POST to errors spec, also changed the example test to be more specific
    Co-Authored-By: Charlie Morris <523381+cdmo@users.noreply.github.com>

--- spec/requests/errors_spec.rb
+++ spec/requests/errors_spec.rb
@@ -3,27 +3,55 @@
 require 'rails_helper'
 
 RSpec.describe 'Errors', type: :request do
-  describe 'not found' do
-    before (:all) { get '/404' }
+  context 'when GET to errors route' do
+    describe 'not found' do
+      before (:all) { get '/404' }
 
-    it 'has http status 404' do
-      expect(response).to have_http_status(:not_found)
+      it 'has http status 404' do
+        expect(response).to have_http_status(:not_found)
+      end
+
+      it 'redirects to customized not_found error page' do
+        expect(response.body).to include '<h4>The page you were looking for doesn\'t exist.</h4>'
+      end
     end
 
-    it 'redirects to customized not_found error page' do
-      expect(response.body).to include("The page you were looking for doesn't exist")
+    describe 'internal server error' do
+      before (:all) { get '/500' }
+
+      it 'has http status 500' do
+        expect(response).to have_http_status(:internal_server_error)
+      end
+
+      it 'redirects to customized internal_server_error error page' do
+        expect(response.body).to include '<h4>We\'re sorry, but something went wrong.</h4>'
+      end
     end
   end
 
-  describe 'internal server error' do
-    before (:all) { get '/500' }
+  context 'when POST to errors route' do
+    describe 'not found' do
+      before (:all) { post '/404' }
 
-    it 'has http status 500' do
-      expect(response).to have_http_status(:internal_server_error)
+      it 'has http status 404' do
+        expect(response).to have_http_status(:not_found)
+      end
+
+      it 'redirects to customized not_found error page' do
+        expect(response.body).to include '<h4>The page you were looking for doesn\'t exist.</h4>'
+      end
     end
 
-    it 'redirects to customized internal_server_error error page' do
-      expect(response.body).to include("We're sorry, but something went wrong.")
+    describe 'internal server error' do
+      before (:all) { post '/500' }
+
+      it 'has http status 500' do
+        expect(response).to have_http_status(:internal_server_error)
+      end
+
+      it 'redirects to customized internal_server_error error page' do
+        expect(response.body).to include '<h4>We\'re sorry, but something went wrong.</h4>'
+      end
     end
   end
 end
