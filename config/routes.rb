bbf79cb75cd987afcaeae31deb99397c504c28fd
Charlie Morris <523381+cdmo@users.noreply.github.com>
2020-03-05 13:53:12 -0500

    custom error msgs for 404 and 500s (#139)

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,12 @@ Rails.application.routes.draw do
   get 'holds/result', to: 'holds#result', as: :result
 
   get '/logout', to: 'sessions#destroy', as: :logout
+
+  # error pages
+  match '404', to: 'errors#not_found', via: :all
+  match '422', to: 'errors#not_found', via: :all
+  match '500', to: 'errors#internal_server_error', via: :all
+
+  # catchall for not predefined requests - keep this at the very bottom of the routes file
+  match '*catch_unknown_routes', to: 'errors#not_found', via: :all
 end
