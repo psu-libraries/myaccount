3be82d94f0d4539e46ef5129009a97a0897306c3
Charlie Morris <cdm32@psu.edu>
2020-04-24 22:50:48 -0400

    I am reverting this change because though it works in the browser I use, it breaks in the feature spec for all browsers: selenium and selenium chrome (head or headless for both) :shrug:

--- app/javascript/submission_handling/shared.js
+++
@@ -1,5 +0,0 @@
-export const pendingHoldsForm = document.querySelector('form#pending-holds');
-export const pendingHoldsFormCheckboxes = document.querySelectorAll('form#pending-holds .checkbox:checked');
-export const spinner = `<div class="spinner-border" role="status">
-                   <span class="sr-only">Loading...</span>
-                 </div>`;
