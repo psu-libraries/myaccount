6b53b35c8f74f3187bfa4a36086871943eb1498d
Charlie Morris <cdm32@psu.edu>
2020-04-24 22:55:04 -0400

    Specs

--- app/jobs/change_pickup_by_date_job.rb
+++ app/jobs/change_pickup_by_date_job.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class ChangePickupByDateJob < ApplicationJob
   queue_as :default
 
@@ -6,19 +8,18 @@ class ChangePickupByDateJob < ApplicationJob
     redis_client = Redis.new
 
     response = symphony_client.not_needed_after(
-          hold_key: hold_key,
-          fill_by_date: pickup_by_date,
-          session_token: session_token
-      )
+      hold_key: hold_key,
+      fill_by_date: pickup_by_date,
+      session_token: session_token
+    )
 
     case response.status
     when 200
       redis_client.set("pickup_by_date_#{hold_key}", {
-          hold_id: hold_key,
-          result: :success,
-          new_value: pickup_by_date
+        hold_id: hold_key,
+        result: :success,
+        new_value: pickup_by_date
       }.to_json)
-      Sidekiq.logger.info 'success!'
     else
       Sidekiq.logger.error("pickup_by_date_#{hold_key}: #{response.body}")
       redis_client.set("pickup_by_date_#{hold_key}", hold_id: hold_key, result: :failure, response: response.body)
