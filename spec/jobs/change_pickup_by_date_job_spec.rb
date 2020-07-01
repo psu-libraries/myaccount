255e272f82a81e1afbc179440720ba231adcd394
Charlie Morris <cdm32@psu.edu>
2020-07-01 10:45:39 -0400

    pickup date change spec

--- spec/jobs/change_pickup_by_date_job_spec.rb
+++ spec/jobs/change_pickup_by_date_job_spec.rb
@@ -4,15 +4,10 @@ require 'rails_helper'
 
 RSpec.describe ChangePickupByDateJob, type: :job do
   describe '#perform_later' do
-    let(:mock_sc_client) { instance_double(SymphonyClient) }
-    let(:ws_args) { { hold_key: 1,
-                      pickup_by_date: '2010-01-01',
-                      session_token: '1s2fa21465' } }
-    let(:sc_response) { instance_double(HTTP::Response, status: 200) }
+    let(:ws_args) { { hold_key: 1, session_token: '1s2fa21465', pickup_by_date: '20201212' } }
 
     before do
-      allow(SymphonyClient).to receive(:new).and_return(mock_sc_client)
-      allow(mock_sc_client).to receive(:not_needed_after).and_return(sc_response)
+      stub_request(:any, /example.com/).to_rack(FakeSymphony)
     end
 
     after do
@@ -22,8 +17,9 @@ RSpec.describe ChangePickupByDateJob, type: :job do
     context 'with valid input' do
       it 'makes a call to the SymphonyClient' do
         described_class.perform_now(**ws_args)
+        results = Redis.current.get 'pickup_by_date_1'
 
-        expect(mock_sc_client).to have_received(:not_needed_after)
+        expect(results).to include('success')
       end
     end
 
@@ -39,26 +35,21 @@ RSpec.describe ChangePickupByDateJob, type: :job do
         described_class.perform_now(**ws_args)
         results = Redis.current.get 'pickup_by_date_1'
 
-        expect(results).to include 'January 1, 2010'
+        expect(results).to include 'December 12, 2020'
       end
     end
 
     context 'with valid input that is returned not OK from SymphonyClient' do
       before do
-        allow(sc_response).to receive(:status).and_return 500
-        allow(sc_response).to receive(:body).and_return({
-          messageList: [{
-            code: 'some_error_code',
-            message: 'Some error message'
-          }]
-        }.to_json)
+        stub_request(:put, 'https://example.com/symwsbc/circulation/holdRecord/key/1')
+            .to_return(status: 500, body: '{ "messageList": [{ "message": "A bad thing happened" }] }', headers: {})
       end
 
       it 'makes a record of the failure' do
         described_class.perform_now(**ws_args)
         results = Redis.current.get 'pickup_by_date_1'
 
-        expect(results).to eq '{"hold_id":1,"result":"failure","response":"Some error message"}'
+        expect(JSON.parse results).to include "id","result","response","display_error"
       end
     end
   end
