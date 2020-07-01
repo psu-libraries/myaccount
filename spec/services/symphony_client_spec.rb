3dfd3924aa3fea02423fddbfe3091de85401ddbe
Charlie Morris <cdm32@psu.edu>
2020-07-01 11:38:40 -0400

    Fix signature

--- spec/services/symphony_client_spec.rb
+++ spec/services/symphony_client_spec.rb
@@ -399,7 +399,7 @@ RSpec.describe SymphonyClient do
     let(:include_fields) { '*,bib{shadowed,title,author},call{*}' }
 
     it 'returns the resource item record' do
-      item_response = client.get_item_info(barcode, user.session_token)
+      item_response = client.get_item_info(barcode, user.session_token, nil)
 
       expect(JSON.parse(item_response)).to include 'resource' => '/catalog/item'
     end
