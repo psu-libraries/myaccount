b41940ba38b40027961eef06e48e261b5775c168
Charlie Morris <cdm32@psu.edu>
2020-07-26 14:56:12 -0400

    Site chrome

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,26 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def nav_link_attributes(path)
+    {
+      class: "nav-link #{('active' if active_controller?(path))}",
+      aria: {
+        current: ('page' if active_controller?(path))
+      }
+    }
+  end
+
+  def derive_path(path_string)
+    send "#{path_string}_path"
+  end
+
+  def current_year
+    Date.today.year
+  end
+
+  private
+
+    def active_controller?(name)
+      name == controller_name
+    end
 end
---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe ApplicationHelper, type: :helper do
+  describe '#derive_path' do
+    it 'calls the Rails path method that returns a route' do
+      expect(helper.derive_path('summaries')).to eq '/summaries'
+    end
+  end
+
+  describe '#nav_link_attributes' do
+    it 'produces user-friendly features indicating current location' do
+      expect(helper.nav_link_attributes('test')).to eq({ aria: { current: 'page' }, class: 'nav-link active' })
+    end
+  end
+
+  describe '#current_year' do
+    let(:some_day) { Date.parse('21000203') }
+
+    before do
+      allow(Date).to receive(:today).and_return(some_day)
+    end
+
+    it 'returns the current year' do
+      expect(helper.current_year).to eq 2100
+    end
+  end
+end
