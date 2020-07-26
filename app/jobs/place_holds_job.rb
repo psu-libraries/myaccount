0e3a7680e6a199ce10a7071bd6fdeeed17814592
Charlie Morris <cdm32@psu.edu>
2020-07-26 09:58:43 -0400

    Refactored to be easier to read

--- app/jobs/place_holds_job.rb
+++ app/jobs/place_holds_job.rb
@@ -4,13 +4,45 @@ class PlaceHoldsJob < ApplicationJob
   queue_as :default
 
   def perform(patron_key:, barcodes:, session_token:, **hold_args)
-    symphony_client = SymphonyClient.new
-    patron_response = symphony_client.patron_info patron_key: patron_key,
-                                                  session_token: session_token
+    @symphony_client = SymphonyClient.new
+    @barcodes = barcodes
+    @session_token = session_token
+    @hold_args = hold_args
+    @patron = fetch_patron patron_key
 
-    patron = Patron.new patron_response
-    results = barcodes.each_with_object(success: [], error: []) do |barcode, status|
-      response = symphony_client.place_hold(patron, session_token, barcode, hold_args)
+    set_redis_values print_html(batch_results)
+  end
+
+  private
+
+  def set_redis_values(html)
+    Redis.current.set("place_holds_results_#{@patron.key}", {
+        id: @patron.barcode.to_s,
+        result: :success,
+        response: html
+    }.to_json)
+  end
+
+  def print_html(results)
+    results_builder = PlaceHoldResults::Builder.new(user_token: @session_token,
+                                                    client: @symphony_client,
+                                                    place_hold_results: results)
+
+    ApplicationController.render partial: 'holds/results',
+                                layout: false,
+                                locals: { place_hold_results: results_builder.generate }
+  end
+
+  def fetch_patron(patron_key)
+    patron_response = @symphony_client.patron_info patron_key: patron_key,
+                                                  session_token: @session_token
+    Patron.new patron_response
+  end
+
+
+  def batch_results
+    @barcodes.each_with_object(success: [], error: []) do |barcode, status|
+      response = @symphony_client.place_hold(@patron, @session_token, barcode, @hold_args)
 
       sirsi_response = SirsiResponse.new response
 
@@ -18,25 +50,11 @@ class PlaceHoldsJob < ApplicationJob
         status[:success] << { barcode: barcode,
                               hold_key: sirsi_response.hold.key }
       else
-        Sidekiq.logger.error("place_holds_results_#{patron_key} #{barcode}: #{sirsi_response.response_raw}")
+        Sidekiq.logger.error("place_holds_results_#{@patron_key} #{barcode}: #{sirsi_response.response_raw}")
         status[:error] << { barcode: barcode,
                             result: :failure,
                             error_message: sirsi_response.messages.first[:message] }
       end
     end
-
-    results_builder = PlaceHoldResults::Builder.new(user_token: session_token,
-                                                    client: symphony_client,
-                                                    place_hold_results: results)
-
-    html = ApplicationController.render partial: 'holds/results',
-                                        layout: false,
-                                        locals: { place_hold_results: results_builder.generate }
-
-    Redis.current.set("place_holds_results_#{patron_key}", {
-      id: patron.barcode.to_s,
-      result: :success,
-      response: html
-    }.to_json)
   end
 end
