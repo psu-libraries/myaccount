b5185d786ec1102c354e8e068a77b25ed7720c5e
Charlie Morris <523381+cdmo@users.noreply.github.com>
2020-09-28 15:59:51 -0400

    Caches the version for 5 minutes and refactors class (#270)
    * Caches the version for 5 minutes and refactors class

--- app/lib/myaccount_version.rb
+++ app/lib/myaccount_version.rb
@@ -1,18 +1,28 @@
 # frozen_string_literal: true
 
 module MyaccountVersion
-  def self.version
-    version_from_github || 'not set'
+  def self.resolve_version
+    set_version
+    Redis.current.get 'myaccount_version'
+  end
+
+  VERSION_CACHE_TIME = 300
+  VERSION_NOT_SET_NAME = 'Version could not be determined'
+
+  # `nx: true` means that Redis will only set this value if it doesn't exist. The `ex` option tells Redis when to delete
+  # the key.
+  def self.set_version
+    Redis.current.set 'myaccount_version', version_from_github, ex: VERSION_CACHE_TIME, nx: true
   end
 
   def self.version_from_github
     client = Octokit::Client.new
     releases = client&.releases 'psu-libraries/myaccount'
 
-    return unless good_response? releases
+    return VERSION_NOT_SET_NAME unless good_response? releases
 
     # Strange notation below because this is not a Hash it is a Sawyer::Response
-    releases&.first&.[] :name
+    releases&.first&.[] :name || VERSION_NOT_SET_NAME
   end
 
   # Weirdly, releases returns a String like "{}" OR an Array of Sawyer objects. :shrug:
