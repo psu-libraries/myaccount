79c93cf97e08e43646483f5598f99bfef4c0102d
Banu Kutlu <bzk60@psu.edu>
2020-10-07 15:00:20 -0400

    moving pickup_locations_actual to settings.yml

--- app/models/hold.rb
+++ app/models/hold.rb
@@ -6,55 +6,7 @@ class Hold
 
   PICKUP_LOCATION_REQUESTED = Settings.pickup_locations.to_h.invert
 
-  PICKUP_LOCATION_ACTUAL = {
-    ABINGTON: 'Abington',
-    ACQ_DSL: 'Acquisitions (Law Schools)',
-    ACQUISTNS: 'Acquisitions (UP)',
-    ALTOONA: 'Altoona',
-    BEAVER: 'Beaver',
-    BEHREND: 'Erie',
-    BERKS: 'Berks',
-    BRANDYWINE: 'Brandywine',
-    "DSL-CARL": 'Dickinson Law (Carlisle)',
-    "DSL-UP": 'Penn State Law (UP)',
-    DUBOIS: 'DuBois',
-    FAYETTE: 'Fayette',
-    GALLEGHENY: 'Greater Allegheny',
-    GREATVLY: 'Great Valley',
-    HARRISBURG: 'Harrisburg',
-    HAZLETON: 'Hazleton',
-    HERSHEY: 'Hershey (College of Medicine)',
-    LEHIGHVLY: 'Lehigh Valley',
-    MONTALTO: 'Mont Alto',
-    NEWKEN: 'New Kensington',
-    ONLINE: 'Online Resource',
-    SCHUYLKILL: 'Schuylkill',
-    "SERIAL-SRV": 'Acquisitions (UP)',
-    SHENANGO: 'Shenango',
-    "UP-ANNEX": 'Library Storage',
-    "UP-ARCHIT": 'Architecture & Landscape Architecture Library (UP)',
-    "UP-ARTSHUM": 'Arts & Humanities Library (Pattee)',
-    "UP-BUSINES": 'Business Library (Paterno)',
-    "UP-EMS": 'Earth & Mineral Sciences Library (UP)',
-    "UP-ENGIN": 'Engineering Library (UP)',
-    "UP-GATEWAY": 'Global Gateways',
-    "UP-LIFESCI": 'Life Sciences Library (Paterno)',
-    "UP-MAPS": 'Maps and Geospatial Information (Pattee)',
-    "UP-MEDICAL": 'College of Medicine (UP)',
-    "UP-MICRO": 'Microforms Library (Paterno)',
-    "UP-OFFICE": 'Employee Office',
-    "UP-PAMS": 'Physical & Mathematical Sciences Library (UP)',
-    "UP-PAT": 'Pattee Library and Paterno Library Stacks',
-    "UP-SOCSCI": 'Social Sciences & Education Library (Paterno)',
-    "UP-EDUBEHV": 'Social Sciences & Education Library (Paterno)',
-    "UP-SPECCOL": 'Special Collections Library (Paterno)',
-    WILKESBAR: 'Wilkes-Barre',
-    WITHDRAWN: 'Withdrawn',
-    WORLD: 'World Campus',
-    WSCRANTON: 'Scranton',
-    XTERNAL: 'Associated Libraries',
-    YORK: 'York'
-  }.with_indifferent_access
+  PICKUP_LOCATION_ACTUAL = Settings.pickup_locations_actual.to_h
 
   attr_reader :record
 
@@ -75,7 +27,7 @@ class Hold
   end
 
   def pickup_library_human
-    PICKUP_LOCATION_ACTUAL[pickup_library_code] || pickup_library_code
+    PICKUP_LOCATION_ACTUAL[pickup_library_code.to_sym] || pickup_library_code
   end
 
   def expiration_date
