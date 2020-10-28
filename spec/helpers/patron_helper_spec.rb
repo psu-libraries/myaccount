7117b40f2606611661e9bbb93077fd5c873cd300
Charlie Morris <523381+cdmo@users.noreply.github.com>
2020-10-27 23:07:59 -0400

    Adds a default 'Select a state' option for US State dropdown (#309)

--- spec/helpers/patron_helper_spec.rb
+++ spec/helpers/patron_helper_spec.rb
@@ -3,9 +3,23 @@
 require 'rails_helper'
 
 RSpec.describe PatronHelper, type: :helper do
-  describe '#us_states' do
-    it 'contains a simple dictionary of US States and their abbreviations' do
-      expect(helper.us_states['Arizona']).to eq 'AZ'
+  describe '#render_us_states_dropdown' do
+    context 'when patron does not have a state to be found' do
+      it 'renders an options set with the default selected and disabled value of "Select a state"' do
+        results = helper.render_us_states_dropdown(selected: nil)
+
+        expect(results).to include('<option selected="selected" disabled="disabled" value="">Select a state</option>')
+      end
+    end
+
+    context 'when patron has a state in their address' do
+      it 'renders an options set with the default selected value of the passed in state acronym' do
+        results = helper.render_us_states_dropdown(selected: 'PA')
+
+        expect(results).not_to include('<option selected="selected" disabled="disabled" value="">Select a state'\
+                                       '</option>')
+        expect(results).to include('<option selected="selected" value="PA">Pennsylvania</option>')
+      end
     end
   end
 end
