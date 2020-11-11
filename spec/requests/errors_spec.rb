3612c1a7683731928a3b938ff8011806ee2308a8
Banu Kutlu <bzk60@psu.edu>
2020-11-11 12:01:12 -0500

    fix for unauthenticated 404 requests

--- spec/requests/errors_spec.rb
+++ spec/requests/errors_spec.rb
@@ -3,73 +3,83 @@
 require 'rails_helper'
 
 RSpec.describe 'Errors', type: :request do
-  let(:mock_user) { 'patron2' }
+  RSpec.shared_examples 'error examples' do
+    context 'when GET to errors route' do
+      describe 'not found' do
+        before { get '/bad_route' }
 
-  before do
-    login_permanently_as username: 'PATRON2', patron_key: mock_user
+        it 'has http status 404' do
+          expect(response).to have_http_status(:not_found)
+        end
+
+        it 'redirects to customized not_found error page' do
+          expect(response.body).to include '<h4>The page you were looking for doesn\'t exist.</h4>'
+        end
+      end
+
+      describe 'not found with formats other than html' do
+        before { get '/bad_route.xml' }
+
+        it 'has http status 404' do
+          expect(response).to have_http_status(:not_found)
+        end
+
+        it 'redirects to customized not_found error page' do
+          expect(response.body).to include '<h4>The page you were looking for doesn\'t exist.</h4>'
+        end
+      end
+
+      describe 'internal server error' do
+        before { get '/500' }
+
+        it 'has http status 500' do
+          expect(response).to have_http_status(:internal_server_error)
+        end
+
+        it 'redirects to customized internal_server_error error page' do
+          expect(response.body).to include '<h4>We\'re sorry, but something went wrong.</h4>'
+        end
+      end
+    end
+
+    context 'when POST to errors route' do
+      describe 'not found' do
+        before { post '/404' }
+
+        it 'has http status 404' do
+          expect(response).to have_http_status(:not_found)
+        end
+
+        it 'redirects to customized not_found error page' do
+          expect(response.body).to include '<h4>The page you were looking for doesn\'t exist.</h4>'
+        end
+      end
+
+      describe 'internal server error' do
+        before { post '/500' }
+
+        it 'has http status 500' do
+          expect(response).to have_http_status(:internal_server_error)
+        end
+
+        it 'redirects to customized internal_server_error error page' do
+          expect(response.body).to include '<h4>We\'re sorry, but something went wrong.</h4>'
+        end
+      end
+    end
   end
 
-  context 'when GET to errors route' do
-    describe 'not found' do
-      before { get '/404' }
-
-      it 'has http status 404' do
-        expect(response).to have_http_status(:not_found)
-      end
-
-      it 'redirects to customized not_found error page' do
-        expect(response.body).to include '<h4>The page you were looking for doesn\'t exist.</h4>'
-      end
-    end
-
-    describe 'not found with formats other than html' do
-      before { get '/404.xml' }
-
-      it 'has http status 404' do
-        expect(response).to have_http_status(:not_found)
-      end
-
-      it 'redirects to customized not_found error page' do
-        expect(response.body).to include '<h4>The page you were looking for doesn\'t exist.</h4>'
-      end
-    end
-
-    describe 'internal server error' do
-      before { get '/500' }
-
-      it 'has http status 500' do
-        expect(response).to have_http_status(:internal_server_error)
-      end
-
-      it 'redirects to customized internal_server_error error page' do
-        expect(response.body).to include '<h4>We\'re sorry, but something went wrong.</h4>'
-      end
-    end
+  context 'with an unauthenticated request' do
+    include_examples 'error examples'
   end
 
-  context 'when POST to errors route' do
-    describe 'not found' do
-      before { post '/404' }
+  context 'with an authenticated request' do
+    let(:mock_user) { 'patron2' }
 
-      it 'has http status 404' do
-        expect(response).to have_http_status(:not_found)
-      end
-
-      it 'redirects to customized not_found error page' do
-        expect(response.body).to include '<h4>The page you were looking for doesn\'t exist.</h4>'
-      end
+    before do
+      login_permanently_as username: 'PATRON2', patron_key: mock_user
     end
 
-    describe 'internal server error' do
-      before { post '/500' }
-
-      it 'has http status 500' do
-        expect(response).to have_http_status(:internal_server_error)
-      end
-
-      it 'redirects to customized internal_server_error error page' do
-        expect(response.body).to include '<h4>We\'re sorry, but something went wrong.</h4>'
-      end
-    end
+    include_examples 'error examples'
   end
 end
--- spec/views/layouts/application.html.erb_spec.rb
+++ spec/views/layouts/application.html.erb_spec.rb
@@ -3,8 +3,6 @@
 require 'rails_helper'
 
 RSpec.describe 'layouts/application', type: :view do
-  let(:mock_patron) { instance_double Patron, id: 'idhere' }
-
   before do
     without_partial_double_verification do
       allow(view).to receive(:patron).and_return(mock_patron)
@@ -12,32 +10,52 @@ RSpec.describe 'layouts/application', type: :view do
     allow(mock_patron).to receive(:valid?).and_return(true)
   end
 
-  context 'when site is not in maintenance mode' do
-    after do
-      Settings.maintenance_mode = false
-    end
+  context 'with an authenticated request' do
+    let(:mock_patron) { instance_double Patron, id: 'idhere' }
 
-    it 'does not add the system maintenance notice' do
-      assign('patron', instance_double(Patron, id: 'idhere'))
-
+    it 'displays the navigation' do
       render
 
-      expect(rendered).not_to have_text 'System Maintenance'
+      expect(rendered).to have_css '#navbarNav'
+    end
+
+    context 'when site is not in maintenance mode' do
+      after do
+        Settings.maintenance_mode = false
+      end
+
+      it 'does not add the system maintenance notice' do
+        assign('patron', instance_double(Patron, id: 'idhere'))
+
+        render
+
+        expect(rendered).not_to have_text 'System Maintenance'
+      end
+    end
+
+    context 'when site is in maintenance mode' do
+      after do
+        Settings.maintenance_mode = false
+      end
+
+      it 'adds the system maintenance notice' do
+        assign('patron', instance_double(Patron, id: 'idhere'))
+        Settings.maintenance_mode = true
+
+        render
+
+        expect(rendered).to have_text 'System Maintenance'
+      end
     end
   end
 
-  context 'when site is in maintenance mode' do
-    after do
-      Settings.maintenance_mode = false
-    end
+  context 'with an unauthenticated request' do
+    let(:mock_patron) { nil }
 
-    it 'adds the system maintenance notice' do
-      assign('patron', instance_double(Patron, id: 'idhere'))
-      Settings.maintenance_mode = true
-
+    it 'does not display the navigation' do
       render
 
-      expect(rendered).to have_text 'System Maintenance'
+      expect(rendered).not_to have_css '#navbarNav'
     end
   end
 end
