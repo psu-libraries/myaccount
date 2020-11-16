36e09aaeb0e2906a70fa66581e6f3b20bad5e3ae
Banu Hapeloglu Kutlu <bzk60@psu.edu>
2020-11-16 14:54:07 -0500

    Update app/controllers/errors_controller.rb
    Co-authored-by: Charlie Morris <523381+cdmo@users.noreply.github.com>

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -3,8 +3,7 @@
 class ErrorsController < ApplicationController
   def not_found
     respond_to do |format|
-      format.html { render status: 404 }
-      format.all { render status: 404, formats: :html, content_type: 'text/html' }
+      format.any { render status: 404, formats: :html, content_type: 'text/html' }
     end
   end
 
