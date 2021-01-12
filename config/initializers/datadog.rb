cf107cb36010c645cef387fa9a11d9ce296fb2e1
Banu Kutlu <bzk60@psu.edu>
2021-01-12 16:29:45 -0500

    moving around

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -10,4 +10,25 @@ if Settings&.datadog&.on_server
     c.use :sidekiq, service_name: 'myaccount-sidekiq'
     c.use :redis, service_name: 'myaccount-redis'
   end
+
+  # Monkey patching Datadog
+  Datadog::Contrib::Httprb::Instrumentation::InstanceMethods.module_eval do
+    private
+
+      def annotate_span_with_response!(span, response)
+        return unless response&.code
+
+        span.set_tag(Datadog::Ext::HTTP::STATUS_CODE, response.code)
+
+        case response.code.to_i
+        when 400...599
+          begin
+            message = JSON.parse(response.body)['messageList']
+          rescue StandardError
+            message = 'Error'
+          end
+          span.set_error(["Error #{response.code}", message]) unless message.first['code'] == 'hatErrorResponse.116'
+        end
+      end
+  end
 end
