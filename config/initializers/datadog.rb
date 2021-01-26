324ff570ee9a4c6b63f0bd4ef49eee7a7acee17c
Banu Hapeloglu Kutlu <bzk60@psu.edu>
2021-01-26 09:17:54 -0500

    Tell datadog to ignore "records in use" errors (#341)
    Monkey patching datadog rb client to not report hatErrorResponse.116 records in use error

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -10,4 +10,26 @@ if Settings&.datadog&.on_server
     c.use :sidekiq, service_name: 'myaccount-sidekiq'
     c.use :redis, service_name: 'myaccount-redis'
   end
+
+  # Monkey patching Datadog
+  Datadog::Contrib::Httprb::Instrumentation::InstanceMethods.module_eval do
+    private
+
+      def annotate_span_with_response!(span, response)
+        return unless response&.code
+
+        span.set_tag(Datadog::Ext::HTTP::STATUS_CODE, response.code)
+
+        case response.code.to_i
+        when 400...599
+          message = parsed_response response
+          code = message&.first&.dig('code')
+          span.set_error(["Error #{response.code}", message]) unless code == 'hatErrorResponse.116'
+        end
+      end
+
+      def parsed_response(response)
+        JSON.parse(response.body)&.dig('messageList') || 'Error'
+      end
+  end
 end
