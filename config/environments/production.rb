8c6ac1b44f91a5f64c0ed10607c1cd109e8c756f
whereismyjetpack <djb44@psu.edu>
2021-08-19 11:15:11 -0400

    remove misleading comment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,6 @@ Rails.application.configure do
   # Send deprecation notices to registered listeners.
   # config.active_support.deprecation = :notify
 
-  # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = Lograge::Formatters::Json.new
 
   # Use a different logger for distributed setups.
