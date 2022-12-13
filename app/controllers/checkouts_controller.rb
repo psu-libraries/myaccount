1a3ced98b2cc9f7b5cf2ea4b2ea5c8010594a32b
Alex Kiessling <ajkiessl@gmail.com>
2022-12-13 17:25:40 -0500

    Naming changes

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -12,7 +12,7 @@ class CheckoutsController < ApplicationController
     @username = current_user.username
     ws_args = { patron_key: current_user.patron_key, session_token: current_user.session_token }
     ViewCheckoutsJob.perform_later **ws_args
-    ViewIllLoansJob.perform_later(@username, :checkouts)
+    ViewIlliadLoansJob.perform_later(@username, :checkouts)
 
     @patron_key = current_user.patron_key
     render
--- app/jobs/view_illiad_loans_job.rb
+++ app/jobs/view_illiad_loans_job.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
-class ViewIllLoansJob < ApplicationJob
+class ViewIlliadLoansJob < ApplicationJob
   include ActionController::Rendering
 
   queue_as :default
 
   # 'type' can be either :holds or :checkouts
   def perform(webaccess_id, type)
-    ill_loans = IlliadClient.new.send("get_loan_#{type}", webaccess_id)
+    illiad_loans = IlliadClient.new.send("get_loan_#{type}", webaccess_id)
 
-    html = HoldsController.render template: "#{type}/ill_#{type}", layout: false, locals: { ill_loans: ill_loans }
+    html = HoldsController.render template: "#{type}/ill_#{type}", layout: false, locals: { illiad_loans: illiad_loans }
 
     Redis.current.set("view_ill_#{type}_#{webaccess_id}", {
       result: :success,
