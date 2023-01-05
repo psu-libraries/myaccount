508693294c2cb4f7195ae4fb05a69b31ecbed790
Sarah Smulligan <64047083+Smullz622@users.noreply.github.com>
2023-01-05 11:33:23 -0500

    Accept lending policy rebase (#569)
    * added error checking to lending policy controller

--- app/controllers/lending_policy_controller.rb
+++ app/controllers/lending_policy_controller.rb
@@ -4,13 +4,16 @@ class LendingPolicyController < ApplicationController
   before_action :authenticate_user!
 
   def accept
-    if patron.eligible_for_wage_garnishment?
-      client = SymphonyClient.new
-      client.accept_lending_policy(patron: patron, session_token: current_user.session_token)
-    end
-
     # even if the patron is not eligible for wage garnishment (already opted in or some other reason),
     # just redirect them to the thank you path to avoid further user confusion
-    redirect_to lending_policy_thank_you_path
+    return redirect_to lending_policy_thank_you_path unless patron.eligible_for_wage_garnishment?
+
+    client = SymphonyClient.new
+    response = client.accept_lending_policy(patron: patron, session_token: current_user.session_token)
+    if response.status == 200
+      redirect_to lending_policy_thank_you_path
+    else
+      redirect_to lending_policy_error_path
+    end
   end
 end
