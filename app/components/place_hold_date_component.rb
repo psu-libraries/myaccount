fadc535beffcb364db76e34e2b9737ecbe758a9e
Sarah Smulligan <64047083+Smullz622@users.noreply.github.com>
2023-03-29 15:58:43 -0400

    Changes ILL Hold Defaults(#603)
    Unchecks accept_alternate_edition and accept_ebook by default for ILL holds.
    Sets default not needed after date to 45 days for ILL holds, stays 14 days for regular holds.

--- app/components/place_hold_date_component.rb
+++ app/components/place_hold_date_component.rb
@@ -1,15 +1,20 @@
 # frozen_string_literal: true
 
 class PlaceHoldDateComponent < ViewComponent::Base
-  def initialize(validate_pickup_info: true, make_default: true)
+  def initialize(validate_pickup_info: true, make_default: true, is_ill: false)
     @make_default = make_default
     @validate_pickup_info = validate_pickup_info
+    @is_ill = is_ill
   end
 
   def default_pickup_by_date
     return '' unless make_default
 
-    DateTime.now.+(14.days).strftime('%Y-%m-%d')
+    if is_ill
+      DateTime.now.+(45.days).strftime('%Y-%m-%d')
+    else
+      DateTime.now.+(14.days).strftime('%Y-%m-%d')
+    end
   end
 
   def minimum_pickup_by_date
@@ -18,5 +23,5 @@ class PlaceHoldDateComponent < ViewComponent::Base
 
   private
 
-    attr_reader :make_default, :validate_pickup_info
+    attr_reader :make_default, :validate_pickup_info, :is_ill
 end
