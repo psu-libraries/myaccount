1718a5706d3ce878b75c4a583cc231c742daf854
Smulligan <smm5878@psu.edu>
2023-04-03 15:42:03 -0400

    reverting previous changes. Previous changes to ill_controller made this unnecessary.

--- app/services/symphony_client_parser.rb
+++ app/services/symphony_client_parser.rb
@@ -2,9 +2,8 @@
 
 # Responsible for parsing SymphonyClient responses.
 class SymphonyClientParser
-  def self.parsed_response(symphony_client, symphony_call, *params)
-    kwargs = params.first.nil? ? {} : params.first
-    client_response = symphony_client.send(symphony_call, **kwargs)
-    JSON.parse client_response.body
+  def self.parsed_response(symphony_client, symphony_call, **params)
+    client_response = symphony_client.send(symphony_call, **params)
+    JSON.parse(client_response.body)
   end
 end
