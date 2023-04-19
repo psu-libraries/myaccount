30fde278e6df020025a3bea688181fc8e35976b3
whereismyjetpack <djb44@psu.edu>
2023-04-19 15:16:49 -0400

    adds address1 to stubs

--- spec/jobs/view_holds_job_spec.rb
+++ spec/jobs/view_holds_job_spec.rb
@@ -33,7 +33,8 @@ RSpec.describe ViewHoldsJob do
   context 'when SymphonyClient does not respond with 200/OK' do
     before do
       stub_request(:get, 'https://example.com/symwsbc/user/patron/key/patron1?includeFields=*,customInformation{' \
-                         'patronExtendedInformation{*}},holdRecordList%7B*,bib%7Btitle,author,callList%7B*%7D%7D,' \
+                         'patronExtendedInformation{*}},address1,holdRecordList%7B*,' \
+                         'bib%7Btitle,author,callList%7B*%7D%7D,' \
                          'item%7B*,bib%7Bshadowed,title,author%7D,call%7BsortCallNumber,dispCallNumber%7D%7D%7D')
         .to_return(status: 500, body: '{ "messageList": [{ "message": "A bad thing happened" }] }', headers: {})
     end
