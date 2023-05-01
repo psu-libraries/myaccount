d9fae6f57db537b49527df7d2f156d989bb1f209
Alex Kiessling <ajkiessl@gmail.com>
2023-05-01 12:47:04 -0400

    Adds #library to mock_patron

--- spec/controllers/checkouts_controller_spec.rb
+++ spec/controllers/checkouts_controller_spec.rb
@@ -23,7 +23,7 @@ RSpec.describe CheckoutsController do
         session_token: 'e0b5e1a3e86a399112b9eb893daeacfd' }
     end
 
-    let(:mock_patron) { instance_double(Patron, checkouts:) }
+    let(:mock_patron) { instance_double(Patron, checkouts:, library: 'UP-PAT') }
 
     let(:checkouts) { [
       instance_double(Checkout, item_key: '123', resource: 'item', bib_summary: 'Renewal 1 (ABC)', due_date: nil),
