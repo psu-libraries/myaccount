14bd2bab295bd566293e08f1c24c95e10452eb8c
Dann Bohn <djb44@psu.edu>
2023-10-23 14:32:13 -0400

    log_formtter is now logger.formatter

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,7 +2,7 @@
 
 Sidekiq.configure_server do |config|
   config.redis = { url: Settings.redis.sidekiq.uri, size: 15 }
-  config.log_formatter = Sidekiq::Logger::Formatters::JSON.new
+  config.logger.formatter = Sidekiq::Logger::Formatters::JSON.new
 end
 
 Sidekiq.configure_client do |config|
