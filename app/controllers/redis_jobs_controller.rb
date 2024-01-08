a8079f99e21019065692ea4e8a8d53e56970a8bc
Smulligan <smm5878@psu.edu>
2024-01-08 16:29:00 -0500

    removes line that skips token verification. skipping verification created a vulnerabilty to cross site request forgery

--- app/controllers/redis_jobs_controller.rb
+++ app/controllers/redis_jobs_controller.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class RedisJobsController < ApplicationController
-  skip_before_action :verify_authenticity_token
-
   def show
     job_status = Redis.current.get(params[:id]) || { result: :not_found }
     render json: job_status
