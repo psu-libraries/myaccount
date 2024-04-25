21b2071757a51de30440e2c8f751c84b2cc6978a
Dann Bohn <dannbohn@gmail.com>
2024-04-25 09:11:56 -0400

    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class ApplicationController < ActionController::Base
 
   def patron
     return unless current_user?
-    Rollbar.info(current_user)
+    Rollbar.info('this is a test')
 
     @patron ||= Patron.new(patron_info_response)
   end
