158ecd9f211388c0307f4010654bcd26cab6b559
Dann Bohn <dannbohn@gmail.com>
2024-07-15 14:26:05 -0400

    configure sidekiq timeouts

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: Settings.redis.sidekiq.uri, size: 15 }
+  config.redis = { url: Settings.redis.sidekiq.uri, size: 15, network_timeout: Settings.redis.sidekiq.network_timeout, pool_timeout: Settings.redis.sidekiq.pool_timeout }
   config.logger.formatter = Sidekiq::Logger::Formatters::JSON.new
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: Settings.redis.sidekiq.uri, size: 2 }
+  config.redis = { url: Settings.redis.sidekiq.uri, size: 2, network_timeout: Settings.redis.sidekiq.network_timeout, pool_timeout: Settings.redis.sidekiq.pool_timeout }
 end
